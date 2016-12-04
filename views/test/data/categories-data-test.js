27e10b39d8513e2e5ee676c46bae8ad9277aa210
Viki94 <viktoria.nedyalkova94@gmail.com>
2016-12-04 23:44:49 +0200

    Add tests

---
+++ views/test/data/categories-data-test.js
@@ -0,0 +1,130 @@
+/* globals require describe it beforeEach afterEach*/
+
+const chai = require("chai");
+const sinonModule = require("sinon");
+
+let expect = chai.expect;
+
+describe("Test categories data", () => {
+    let sinon;
+    beforeEach(() => {
+        sinon = sinonModule.sandbox.create();
+    });
+
+    class Category {
+        constructor(propeties) {
+            this.name = propeties.name;
+        }
+
+        save() {
+
+        }
+
+        static find() {}
+        static findOne() {}
+    }
+
+    let data = require("../../data/category-data")({
+        Category
+    });
+
+    describe("getAllCategories", () => {
+        afterEach(() => {
+            sinon.restore();
+        });
+
+        it("Sholud return empty array if categories not excist", done => {
+            let categories = [];
+            sinon.stub(Category, "find", (_, cb) => {
+                cb(null, categories);
+            });
+
+            data.getAllCategories()
+                .then(actualcategories => {
+                    expect(actualcategories).to.eql([]);
+                    done();
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        });
+
+        it("Should return 1 category", done => {
+            let categories = ["Something"];
+            sinon.stub(Category, "find", (_, cb) => {
+                cb(null, categories);
+            });
+
+            data.getAllCategories()
+                .then(actualcategories => {
+                    expect(actualcategories).to.eql(categories);
+                    done();
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        });
+
+        it("Expect to return the category", done => {
+            data.getCategoryById(existingCategoryId)
+                .then((actualCategory => {
+                    expect(actualCategory).to.equal(category);
+                    done();
+                }))
+                .catch(err => {
+                    console.log(err);
+                });
+        });
+    });
+
+    describe("createCategory", () => {
+        beforeEach(() => {
+            sinon.stub(Category.prototype, "save", cb => {
+                cb(null);
+            });
+        });
+
+        afterEach(() => {
+            sinon.restore();
+        });
+
+        it("Should save category, when parameters are valid ", done => {
+            let name = "Something";
+
+            data.createCategory(name,)
+                .then(actualCategory => {
+                    expect(actualCategory.name).to.equal(name);
+                    done();
+                });
+        });
+
+        it("Should fail, when name is empty", done => {
+            let name = "";
+
+            data.createCategory(name)
+                .catch(err => {
+                    expect(err);
+                    done();
+                });
+        });
+
+        it("Should fail, when name length is 2 characters long", done => {
+            let name = "AA";
+
+            data.createCategory(name)
+                .catch(err => {
+                    expect(err);
+                    done();
+                });
+        });
+
+        it("Should fail, when name length is 40 characters long", done => {
+            let name = "A".repeat(40);
+
+            data.createCategory(name)
+                .catch(err => {
+                    expect(err);
+                    done();
+                });
+        });
+});
